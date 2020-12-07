8b2c2f5c5579eb17c4e5e5c470cbfe2fc4ecff8f
Fredrick Ojure <fredojure@hotmail.com>
2020-12-07 21:17:15 +0300

    Added username and email validation to register form

--- blazemarketplace/forms.py
+++ blazemarketplace/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
+
+user = get_user_model()
 class ContactForm(forms.Form):
     fullname = forms.CharField(widget= forms.TextInput(attrs={'class': 'form-control', 'id': 'form-fullname', 'placeholder': 'Your Name'}))
     email = forms.CharField(widget= forms.EmailInput(attrs={'class': 'form-control', 'id': 'form-fullname', 'placeholder': 'Your Email'}))
@@ -19,4 +23,26 @@ class RegisterForm(forms.Form):
     username = forms.CharField(widget= forms.TextInput(attrs={'class': 'form-control', 'id': 'form-fullname', 'placeholder': 'Your Name'}))
     email = forms.EmailField(widget= forms.EmailInput(attrs={'class': 'form-control', 'id': 'form-fullname', 'placeholder': 'Your Email'}))
     password = forms.CharField(widget= forms.PasswordInput(attrs={'class': 'form-control', 'id': 'form-fullname', 'placeholder': 'Password'}))
-    password2 = forms.CharField(widget= forms.PasswordInput(attrs={'class': 'form-control', 'id': 'form-fullname', 'placeholder': 'Confirm password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'form-fullname', 'placeholder': 'Confirm password'}))
+    
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password2 != password:
+            raise forms.ValidationError('Passwords must match')
+        return data
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username=username)
+        if qs.exist():
+            raise forms.ValidationError('This username is taken')
+        return username
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError('This email is taken')
+        return email
--- blazemarketplace/views.py
+++ blazemarketplace/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.models import User
 
 from .forms import ContactForm, LoginForm, RegisterForm
 
@@ -36,6 +37,7 @@ def login_page(request):
     }
     return render(request, 'auth/login.html', context)
 
+user = get_user_model()
 def register_page(request):
     form = RegisterForm(request.POST or None)
     if form.is_valid():
@@ -45,6 +47,13 @@ def register_page(request):
         'title': 'Register Page',
         'form': form
     }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
     return render(request, 'auth/register.html', context)
 
 def contact_page(request):
