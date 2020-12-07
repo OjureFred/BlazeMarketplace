6ae48f212f1aefa72570d7ab5615a3379bcf5e3c
Fredrick Ojure <fredojure@hotmail.com>
2020-12-07 16:08:05 +0300

    Added a contact page

--- blazemarketplace/urls.py
+++ blazemarketplace/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home_page
+from .views import home_page, contact_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page, name="home_page"),
+    path('', home_page, name = "home_page"),
+    path('contact_page', contact_page, name='contact_page'),
 ]
--- blazemarketplace/views.py
+++ blazemarketplace/views.py
@@ -5,4 +5,17 @@ def home_page(request):
     context = {
         'title': 'Hello Word!!!'
     }
+
     return render(request, 'home_page.html', context)
+
+def contact_page(request):
+    context = {
+        'title': 'Contact',
+        'content': 'Welcome to contact page'
+    }
+
+    if request.method == 'POST':
+        print(request.POST)
+        print(request.POST.get('fullname'))
+
+    return render(request, 'contacts/view.html', context)
