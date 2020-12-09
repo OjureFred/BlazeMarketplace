8fc57ee400e9355d8ed663d5f77b654ad105d91d
Fredrick Ojure <fredojure@hotmail.com>
2020-12-09 09:53:32 +0300

    Added template and url for detail view

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
-path('list/', views.product_list_view, name = 'products_list'), 
+path('list/', views.product_list_view, name='products_list'),
+path('detail/<pk>/',  views.product_detail_view, name='product_detail'),
 
 ]
 
