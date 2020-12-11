8487c445978c164f1d06daa9bde2102ae4245423
Fredrick Ojure <fredojure@hotmail.com>
2020-12-11 17:30:59 +0300

    Added subtotal field to Cart

--- carts/views.py
+++ carts/views.py
@@ -4,21 +4,15 @@ from .models import Cart
 from products.models import Product
 
 # Create your views here.
-def cart_create(user=None):
-    product = Product.objects.get(id=1)
-    cart_obj = Cart.objects.create(user=None, products=product)
-
 def cart_home(request):
-    request.session['cart_id'] = 12
-    cart_id = request.session.get('cart_id', None)
-    qs = Cart.objects.filter(id = cart_id)
-    if qs.count() == 1:
-        print('Cart ID exists')
-        cart_obj = qs.first()
-    else:
-        cart_obj = cart_create()
-        #request.session['cart_id']= cart_obj.id
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products()
+    total = 0
+    for x in products:
+        total += x.price
+    
+    print(total)
         
-    context = {'title': 'Blazemarket Shopping Cart', 'cart_id': cart_id}
+    context = {'title': 'Blazemarket Shopping Cart', 'cart_obj': cart_obj}
    
     return render(request, 'carts/home.html', context)
