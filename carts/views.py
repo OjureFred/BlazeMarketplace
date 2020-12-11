bbada417bf5b6026e9326a785d979426f38a6921
Fredrick Ojure <fredojure@hotmail.com>
2020-12-11 09:23:58 +0300

    Created homepage for cart

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cart_home(request):
+    context = {'title': Blazemarket Shopping Cart}
+    return render(request, 'carts/home.html', context)
