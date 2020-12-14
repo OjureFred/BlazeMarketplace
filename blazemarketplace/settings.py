9034b42a62549dcb47d958aeb9ccfe71bd143218
Fredrick Ojure <fredojure@hotmail.com>
2020-12-14 11:53:30 +0300

    Made templates adjustment for heroku cloud

--- blazemarketplace/settings.py
+++ blazemarketplace/settings.py
@@ -173,5 +173,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+#Adjusted for heroku cloud
+
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
