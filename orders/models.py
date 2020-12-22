8c7d939dc0474c48186683e78b9dec83b9b2a0a2
Fredrick Ojure <fredojure@hotmail.com>
2020-12-22 09:50:07 +0300

    Ran migrations for Order model

--- orders/models.py
+++ orders/models.py
@@ -9,11 +9,11 @@ class Order(models.Model):
         ('created', 'Created'),
         ('paid', 'Paid'),
         ('shipped', 'Shipped'),
-        ('refunded', Refunded),
+        ('refunded', 'Refunded'),
     )
 
     order_id = models.CharField(max_length=120, blank=True)
-    cart = models.ForeignKey(Cart)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     status = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
     shipping_total = models.DecimalField(default=500.00, max_digits=100, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
