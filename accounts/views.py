bc06947d7dbf0aa6f5c659936457ccf372292462
Fredrick Ojure <fredojure@hotmail.com>
2020-12-23 20:24:35 +0300

    Updated code for login and register user

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.models import User
+from django.utils.http import  is_safe_url
 
 from django.conf.urls.static import static
 
@@ -11,24 +12,32 @@ from .forms import ContactForm, LoginForm, RegisterForm
 # Create your views here.
 def login_page(request):
     form = LoginForm(request.POST or None)
-    print(request.user.is_authenticated)
+    context = {
+        'title': 'Login Page',
+        'form': form
+    }
+    print('User logged in')
+    next_ = request.GET.get('next')
+    next_post = request.POST.get('next')
+    redirect_path = next_ or next_post or None
     if form.is_valid():
         print(form.cleaned_data)
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
         user = authenticate(request, username=username, password=password)
+        print(user)
         if user is not None:
             login(request, user)
-            return redirect('login_page')
+            if is_safe_url(redirect_path, request.get_host()):
+                return redirect(redirect_path)
+            else:
+                return redirect('/')
         else:
             print('Error')
             
 
-    context = {
-        'title': 'Login Page',
-        'form': form
-    }
-    return render(request, 'login.html', context)
+    
+    return render(request, 'accounts/login.html', context)
 
 user = get_user_model()
 def register_page(request):
@@ -47,4 +56,4 @@ def register_page(request):
         password = form.cleaned_data.get('password')
         new_user = User.objects.create_user(username, email, password)
         print(new_user)
-    return render(request, 'register.html', context)
+    return render(request, 'accounts/register.html', context)
--- blazemarketplace/views.py
+++ blazemarketplace/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LogoutView
 
 from django.conf.urls.static import static
 
