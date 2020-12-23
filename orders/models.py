cd0801cc1c10455b3347194166b4544ef6733fc4
Fredrick Ojure <fredojure@hotmail.com>
2020-12-23 09:58:52 +0300

    Added code to go to checkout

--- orders/models.py
+++ orders/models.py
@@ -28,7 +28,7 @@ class Order(models.Model):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
         new_total = cart_total + shipping_total
-        self.order_total = new_total
+        self.total = new_total
         self.save()
         return new_total
 
@@ -43,10 +43,10 @@ def post_save_cart_total(sender, instance, created, *args, **kwargs):
         cart_obj = instance
         cart_total = cart_obj.total
         cart_id = cart_obj.id
-
         qs = Order.objects.filter(cart__id=cart_id)
-        if qs.count == 1:
+        if qs.count() == 1:
             order_obj = qs.first()
+            print('running update')
             order_obj.update_total()
 
 post_save.connect(post_save_cart_total, sender=Cart)
