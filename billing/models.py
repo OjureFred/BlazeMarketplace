1376c3debf55fc0d0e299d70d4b36a999fbaa4de
Fredrick Ojure <fredojure@hotmail.com>
2020-12-24 10:51:42 +0300

    Created Billing model and ran migrations

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 
+from django.contrib.auth.models import User
+
+
+User = settings.AUTH_USER_MODEL
 # Create your models here.
+class BillingProfile(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, unique=True, on_delete=models.CASCADE)
+    email = models.EmailField()
+    active = models.BooleanField(default=True)
+    update = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+def user_created_reciever(sender, instance, created, *args, **kwargs):
+    if created:
+        BillingProfile.objects.get_or_create(user=instance)
+
+post_save.connect(user_created_reciever, sender=User)
--- blazemarketplace/settings.py
+++ blazemarketplace/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'accounts',
+    'billing',
 ]
 
 MIDDLEWARE = [
