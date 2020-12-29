75f9ccc17dd032effb90c6cce165ec03a0b43c6c
Fredrick Ojure <fredojure@hotmail.com>
2020-12-29 18:41:12 +0300

    Added shipping and billing addresses to order and ran migrations

--- orders/models.py
+++ orders/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import pre_save, post_save
 
 import math
 
+from addresses.models import Address
 from carts.models import Cart
 from billing.models import BillingProfile
 
@@ -30,6 +31,8 @@ class OrderManager(models.Manager):
 class Order(models.Model):
     order_id = models.CharField(max_length=120, blank=True)
     billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True, on_delete=models.CASCADE)
+    shipping_address = models.ForeignKey(Address, related_name='shipping_address', null=True, blank=True, on_delete=models.CASCADE)
+    billing_address = models.ForeignKey(Address, related_name='billing_address',null=True, blank=True, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     status = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
     shipping_total = models.DecimalField(default=500.00, max_digits=100, decimal_places=2)
