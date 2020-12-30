00d3cf86e7a25868c829dcc439e32b95d3696d3e
Fredrick Ojure <fredojure@hotmail.com>
2020-12-30 20:02:35 +0300

    Finalized previous address on checkout form

--- addresses/urls.py
+++ addresses/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-path('checkout_address_create', views.checkout_address_create_view, name = 'checkout_address_create'),
+path('checkout_address_create/', views.checkout_address_create_view, name='checkout_address_create'),
+path('checkout_address_reuse/', views.checkout_address_reuse_view, name='checkout_address_reuse'),
 
 ]
